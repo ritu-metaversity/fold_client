54e2b1dfebfb64c0bfdc35d64dc6511ed8775120
sushant-metaversity <sushant@metaversitytechnologies.com>
2023-02-02 10:31:25 +0530

    Add status typography

--- src/components/Deposit/index.tsx
+++ src/components/Deposit/index.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import HomeLayout from "../layout/homeLayout";
 import DepositManually from "./DepositManually";
 import { userServices } from "../../utils/api/user/services";
 import ActivityTable from "../activityLog/activityLogTable";
 import { columnIds, columns } from "./columns";
 import { IconSmall } from "../layout/styledComponents";
+import { colorHex } from "../../utils/constants";
 
 interface DepositListInterface {
   image: string;
   time: string;
-  status: string;
+  status: "Rejected" | "Pending" | "Success";
   amount: number;
 }
+
+const colorStatus = {
+  Rejected: "error.main",
+  Pending: "warning.main",
+  Success: "success.main",
+};
 
 const Deposit = () => {
   const [depositList, setDepositList] = useState<DepositListInterface[]>([]);
@@ -49,6 +56,7 @@ const Deposit = () => {
           rows={
             depositList.map((item: DepositListInterface) => {
               const newItem: any = { ...item };
+              newItem.status = <StatusTypography status={item.status} />;
               newItem.image = (
                 <a target={"_blank"} href={item.image}>
                   <img style={{ width: 50, height: 50 }} src={item.image} />
@@ -64,3 +72,23 @@ const Deposit = () => {
 };
 
 export default Deposit;
+
+export function StatusTypography({
+  status,
+}: {
+  status: DepositListInterface["status"];
+}) {
+  return (
+    <Typography
+      sx={{
+        bgcolor: colorHex.bg3,
+        borderRadius: 1,
+        p: 0.5,
+        width: "min-content",
+      }}
+      color={colorStatus[status]}
+    >
+      {status}
+    </Typography>
+  );
+}
