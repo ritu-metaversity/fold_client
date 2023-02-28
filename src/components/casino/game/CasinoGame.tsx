5eb6f0dc01049664a3ab88fecb464fa24ef1325f
sushant-metaversity <sushant@metaversitytechnologies.com>
2023-02-28 13:18:31 +0530

    Fix id not going

--- src/components/casino/game/CasinoGame.tsx
+++ src/components/casino/game/CasinoGame.tsx
@@ -11,13 +11,19 @@ const CasinoGame = () => {
     <HomeLayout>
       {matches ? (
         <iframe
-          src="https://m2.fawk.app/#/splash-screen/eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW5nbyIsImV4cCI6MTY3NzU3ODI4MywiaWF0IjoxNjc3NDkxODgzfQ.wKQyFKkCwx59-v-4Mzvdx9DvRqsxXDq-jPrMFu0CK0Y/9482?opentable=56767"
+          src={
+            "https://m2.fawk.app/#/splash-screen/eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW5nbyIsImV4cCI6MTY3NzU3ODI4MywiaWF0IjoxNjc3NDkxODgzfQ.wKQyFKkCwx59-v-4Mzvdx9DvRqsxXDq-jPrMFu0CK0Y/9482?opentable=" +
+            id
+          }
           height="1200px"
           width="100%"
         ></iframe>
       ) : (
         <iframe
-          src="https://d2.fawk.app/#/splash-screen/eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW5nbyIsImV4cCI6MTY3NzU3ODI4MywiaWF0IjoxNjc3NDkxODgzfQ.wKQyFKkCwx59-v-4Mzvdx9DvRqsxXDq-jPrMFu0CK0Y/9482?opentable=56767"
+          src={
+            "https://d2.fawk.app/#/splash-screen/eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYW5nbyIsImV4cCI6MTY3NzU3ODI4MywiaWF0IjoxNjc3NDkxODgzfQ.wKQyFKkCwx59-v-4Mzvdx9DvRqsxXDq-jPrMFu0CK0Y/9482?opentable=" +
+            id
+          }
           height="900px"
           width="100%"
         />
