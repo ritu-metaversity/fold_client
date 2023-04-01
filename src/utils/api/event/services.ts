6221e41f4d2d3026f49599eb18dd92b6b58264e3
metaversitytechnologies <111351877+metaversitytechnologies@users.noreply.github.com>
2023-04-01 14:27:47 +0530

    Update services.ts

--- src/utils/api/event/services.ts
+++ src/utils/api/event/services.ts
@@ -32,8 +32,8 @@ export const eventServices = {
   newFancy: async (id: string) => {
     const params: ApiServiceInterface = {
       resource: {
-        URL: "http://89.39.105.69:9001/fancy/:id",
-        // URL: "http://43.205.50.127:9000/fancy/:id",
+       // URL: "http://89.39.105.69:9001/fancy/:id",
+         URL: "http://43.205.50.127:9000/fancy/:id",
         // URL: "fancy/:id",
         METHOD: "GET",
       },
