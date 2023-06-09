f49509ab4fd0310e593fdfebea65e048d4250e43
ritu-metaversity <rituraj@metaversitytechnologies.com>
2023-06-09 18:17:52 +0530

    latest code

--- src/component/FooterForMob/FooterForMob.js
+++ src/component/FooterForMob/FooterForMob.js
@@ -1,49 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import './FooterForMob.css'
-import { Link, Outlet } from 'react-router-dom';
-import { GameAPI } from '../../apis/gameAPI';
-import {FaFacebookF, FaTelegramPlane, FaWhatsapp, FaYoutube, FaTwitter, FaInstagram} from 'react-icons/fa'
+import React, { useEffect, useState } from "react";
+import "./FooterForMob.css";
+import { Link, Outlet } from "react-router-dom";
+import { GameAPI } from "../../apis/gameAPI";
+import {
+  FaFacebookF,
+  FaTelegramPlane,
+  FaWhatsapp,
+  FaYoutube,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 function FooterForMob() {
+  const [footerImage, setFooterImage] = useState([]);
+  const hostName = window.location.host.split(".");
 
-  const [footerImage, setFooterImage] = useState([])
-  const hostName = (window.location.host).split(".");
+  useEffect(() => {
+    GameAPI.FOOTER_IMAGESS().then((res) => {
+      setFooterImage(res?.data);
+    });
+  }, []);
 
-
-  useEffect(()=>{
-    GameAPI.FOOTER_IMAGESS().then((res)=>{
-      setFooterImage(res?.data)
-    })
-  },[])
-  
+  console.log(footerImage, "dsafawfe");
 
   return (
     <div>
       <footer>
-        <div className='footer-menu'>
-            <ul>
-              <li><Link to="/about-us">About</Link></li>
-              <li><Link to="/" >Rules</Link></li>
-              <li><Link to="terms-and-conditions">Terms and Conditions</Link></li>
-              <li><Link to="/responsible-gaming">Responsible Gaming</Link></li>
-            </ul>
+        <div className="footer-menu">
+          <ul>
+            <li>
+              <Link to="/about-us">About</Link>
+            </li>
+            <li>
+              <Link to="/">Rules</Link>
+            </li>
+            <li>
+              <Link to="terms-and-conditions">Terms and Conditions</Link>
+            </li>
+            <li>
+              <Link to="/responsible-gaming">Responsible Gaming</Link>
+            </li>
+          </ul>
 
-            <ul className='footer_icon'>
-              <li><FaFacebookF /></li>
-              <li><FaTelegramPlane/></li>
-              <li><FaWhatsapp/></li>
-              <li><FaYoutube/></li>
-              <li><FaTwitter/></li>
-              <li><FaInstagram/></li>
-            </ul>
-           
+          <ul className="footer_icon">
+            <li>
+              <img
+                src={footerImage?.u_upi}
+                alt="upi"
+                className="img-fluid"
+              />
+            </li>
+            <li>
+              <img
+                src={footerImage?.u_paytm}
+                alt="paytm"
+                className="img-fluid"
+              />
+            </li>
+            <li>
+              <img
+                src={footerImage?.u_phonePe}
+                alt="phonepe"
+                className="img-fluid"
+              />
+            </li>
+            <li>
+              <img
+                src={footerImage?.u_googlePay}
+                alt="GPay"
+                className="img-fluid"
+              />
+            </li>
+          </ul>
         </div>
-        <p className='fotterP'>
-            © Copyright 2023. All Rights Reserved. Powered by {hostName[0]}.
-            </p>
+        <p className="fotterP">
+          © Copyright 2023. All Rights Reserved. Powered by {hostName[0]}.
+        </p>
       </footer>
     </div>
-  )
+  );
 }
 
 export default FooterForMob;
