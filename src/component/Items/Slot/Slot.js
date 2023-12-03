8f12bed54865f8f690c5e82d0dfa6cab26bd8fde
ritu-metaversity <rituraj@metaversitytechnologies.com>
2023-12-03 19:58:41 +0530

    latest changes

--- src/component/Items/Slot/Slot.js
+++ src/component/Items/Slot/Slot.js
@@ -204,7 +204,7 @@ const Slot = () => {
 
       <Modal centered show={show}   onHide={handleClose}>
         <Modal.Body className="casino_modals_body">
-          <CasinoModals type={1}/>
+          <CasinoModals type={"aura"}/>
           <div className="agree_btn">
             <button onClick={handleAgree}>Ok I Agree</button>
             <button onClick={()=>setShow(false)}>No, I Don't Agree</button>
